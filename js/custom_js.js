0806ba3491b2fe4cdcf012e518252e334277bdfa
boogiman1 <boowon1@naver.com>
2022-09-02 03:40:50 +0900

    프로젝트 3 시작 scss로 작업

---
+++ js/custom_js.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded',function(){
+    var AT = document.querySelectorAll('.nav_link .nav_link::after')
+        AT.addEventListener('mouseenter',function(){
+    })
+})
