31a3f445ffe132baccdab29be246105a85895f43
boogiman1 <boowon1@naver.com>
2022-09-02 09:56:09 +0900

    파일 이미지 경로 수정

--- js/custom_js.js
+++ js/custom_js.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
     var AT = document.querySelectorAll('.nav_link .nav_link::after')
         AT.addEventListener('mouseenter',function(){
+            AT.classList.add('on')
     })
 })
